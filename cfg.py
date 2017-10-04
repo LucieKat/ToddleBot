0adea55a24d02b73208ae551c5b5d78e2f124e48
TimdeJonge <t.dejonge@students.uu.nl>
2017-10-04 23:35:06 +0200

    Untrack Config

--- cfg.py
+++
@@ -1,7 +0,0 @@
-HOST = "irc.twitch.tv"                          # the Twitch IRC server
-PORT = 6667                                     # always use port 6667!
-NICK = "toddle_bot"                             # your Twitch username, lowercase
-PASS = "oauth:tw8m492nc4b5x1x17ab5pujlvcc2ym"   # your Twitch OAuth token
-CHAN = ["#dayoungconjo", "#toddle_bot"]           # the channel you want to join
-RATE = (20/30)
-SAVE = 0
